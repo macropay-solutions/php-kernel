4b43bbe858a8878e1c391d464f6b7684196655b3
Pantea Marius-ciclistu <macropay-solutions@users.noreply.github.com>
2026-09-24 23:06:31 +0300

    Move Obvious Factory Folder

--- src/Traitables/MacropaySolutionsKernelDatabaseObviousFactoriesFactory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace MacropaySolutions\Framework\Traitables;
-
-trait MacropaySolutionsKernelDatabaseObviousFactoriesFactory
-{
-    use \MacropaySolutions\Kernel\Support\Traits\Macroable;
-}
